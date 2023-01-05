101ba9bc90761a6c6cf88d675c78713e2baa7199
Santiago Yate <syate@uno27.co>
2023-01-05 08:53:02 -0500

    When creating a record it is saved in the table

--- pages/crud.vue
+++ pages/crud.vue
@@ -1,64 +1,65 @@
 <template>
   <v-row>
-    <!-- Tabla -->
-    <v-col cols="12">
-      <v-data-table
-        :headers="headers"
-        :items="desserts"
-        :items-per-page="5"
-        class="elevation-1"
-      ></v-data-table>
-    </v-col>
     <!-- Formulario -->
     <v-col cols="12">
       <template>
-        <v-form ref="form" v-model="valid" lazy-validation>
+        <v-form
+          ref="form"
+          v-model="isFormValid"
+          @submit.prevent="sendForm(payload)"
+        >
           <v-text-field
-            v-model="name"
+            v-model="payload.name"
             :counter="10"
-            :rules="nameRules"
-            label="Name"
+            :rules="rules.name"
+            label="Nombre"
             required
           ></v-text-field>
 
           <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
+            v-model="payload.email"
+            :rules="rules.email"
+            label="Correo"
             required
           ></v-text-field>
 
           <v-select
-            v-model="select"
-            :items="items"
-            :rules="[(v) => !!v || 'Item is required']"
-            label="Item"
+            v-model="payload.country"
+            :items="items.countrys"
+            :rules="rules.country"
+            label="Pais"
             required
           ></v-select>
 
           <v-checkbox
-            v-model="checkbox"
-            :rules="[(v) => !!v || 'You must agree to continue!']"
-            label="Do you agree?"
+            v-model="payload.isTraveler"
+            :rules="rules.isTraveler"
+            label="¿Selecciono el pais?"
             required
           ></v-checkbox>
 
           <v-btn
-            :disabled="!valid"
+            :disabled="!isFormValid"
             color="success"
             class="mr-4"
-            @click="validate"
+            type="submit"
           >
             Validate
           </v-btn>
-
-          <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
-
-          <v-btn color="warning" @click="resetValidation">
-            Reset Validation
-          </v-btn>
         </v-form>
       </template>
+    </v-col>
+    <!-- Tabla -->
+    <v-col cols="12">
+      <div class="container-table">
+        <v-data-table
+          :headers="table.headers"
+          :items="table.items"
+          :items-per-page="-1"
+          hide-default-footer
+          class="elevation-1"
+        ></v-data-table>
+      </div>
     </v-col>
   </v-row>
 </template>
@@ -67,120 +68,49 @@
 export default {
   data() {
     return {
-      headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "start",
-          sortable: false,
-          value: "name",
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%",
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%",
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%",
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%",
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%",
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%",
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%",
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%",
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%",
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%",
-        },
-      ],
-      valid: true,
-      name: "",
-      nameRules: [
-        (v) => !!v || "Name is required",
-        (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
-      ],
-      email: "",
-      emailRules: [
-        (v) => !!v || "E-mail is required",
-        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
-      ],
-      select: null,
-      items: ["Item 1", "Item 2", "Item 3", "Item 4"],
-      checkbox: false,
+      isFormValid: true,
+      payload: {
+        name: "",
+        email: "",
+        isTraveler: false,
+        country: null,
+      },
+      items: {
+        countrys: ["Colombia", "Argentina", "Peru", "Mexico", "España"],
+      },
+      rules: {
+        name: [
+          (v) => !!v || "El nombre es requerido",
+          (v) =>
+            (v && v.length <= 10) ||
+            "El nombre debe tener menos de 10 caracteres",
+        ],
+        email: [
+          (v) => !!v || "El correo es requerido",
+          (v) => /.+@.+\..+/.test(v) || "El correo es invalido",
+        ],
+        isTraveler: [(v) => !!v || "Es necesario llenar la casilla"],
+        country: [(v) => !!v || "El pais es requerido"],
+      },
+      table: {
+        headers: [
+          {
+            text: "Nombre",
+            align: "start",
+            sortable: false,
+            value: "name",
+          },
+          { text: "Correo", value: "email" },
+          { text: "Pais", value: "country" },
+          { text: "Confirmacion de pais", value: "isTraveler" },
+        ],
+        items: [],
+      },
     };
   },
   methods: {
-    validate() {
-      this.$refs.form.validate();
+    sendForm(payload) {
+      this.table.items.push(payload);
     },
     reset() {
       this.$refs.form.reset();
@@ -191,3 +121,11 @@ export default {
   },
 };
 </script>
+
+<style>
+.container-table {
+  height: 10px;
+  height: 456px;
+  overflow: auto;
+}
+</style>
