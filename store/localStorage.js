b9e5ab17b73da4da3ae52e23d52b9e1114af7024
Santiago Yate <syate@uno27.co>
2023-01-10 09:43:18 -0500

    Local storage configured

---
+++ store/localStorage.js
@@ -0,0 +1,35 @@
+const getDefaultState = () => ({
+  data: [],
+});
+
+export const state = () => ({
+  ...getDefaultState(),
+});
+
+export const mutations = {
+  updateKey(state, payload) {
+    state[payload.object][payload.key] = payload.value;
+  },
+
+  updateValue(state, payload) {
+    state[payload.key] = payload.value;
+  },
+
+  resetState(state) {
+    Object.assign(state, getDefaultState());
+  },
+};
+
+export const actions = {
+  actUpdateKey({ commit }, payload) {
+    commit("updateKey", payload);
+  },
+
+  actUpdateValue({ commit }, payload) {
+    commit("updateValue", payload);
+  },
+
+  actResetState({ commit }) {
+    commit("resetState");
+  },
+};
